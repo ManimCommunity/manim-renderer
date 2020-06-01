d7f57028160802bbdeaa547ae9586bf27216e92a
Devin Neal <devin@eulertour.com>
2020-05-31 20:16:24 -0700

    Basic renderer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <canvas class="renderer-element" ref="renderer" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as THREE from "three";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  created() {
+    this.fps = 15;
+    this.aspectRatio = 16 / 9;
+    this.rendererHeight = 480; // Set to 720 for 720p
+    this.sceneHeight = 8;
+    this.cameraNear = 1; // z = 2
+    this.cameraFar = 5; // z = -2
+    this.cameraZPosition = 3;
+
+    this.scene = null;
+    this.camera = null;
+    this.renderer = null;
+
+    this.frameData = [];
+
+    // Maps Mobject IDs from Python to their respective Mobjects in
+    // Javascript.
+    this.mobjectDict = {};
+  },
+  computed: {
+    sceneWidth() {
+      return this.sceneHeight * this.aspectRatio;
+    },
+    rendererWidth() {
+      return this.rendererHeight * this.aspectRatio;
+    }
+  },
+  mounted() {
+    // Scene
+    this.scene = new THREE.Scene();
+
+    // Camera
+    this.camera = new THREE.OrthographicCamera(
+      /*left=*/ -this.sceneWidth / 2,
+      /*right=*/ this.sceneWidth / 2,
+      /*top=*/ this.sceneHeight / 2,
+      /*bottom=*/ -this.sceneHeight / 2,
+      /*near=*/ this.cameraNear,
+      /*far=*/ this.cameraFar
+    );
+    this.camera.position.z = this.cameraZPosition;
+
+    // Renderer
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.renderer,
+      antialias: true
+    });
+    this.renderer.setSize(
+      this.rendererHeight * this.aspectRatio,
+      this.rendererHeight,
+      false
+    );
+    document.body.appendChild(this.renderer.domElement);
+
+    // Add something.
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+
+    // Animate it.
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      this.renderer.render(this.scene, this.camera);
+    };
+    animate();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
